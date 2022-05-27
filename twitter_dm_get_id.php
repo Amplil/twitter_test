0f1e78c6a33c87d94392dd8d7b6d028017f93efa
amplil <aikotobahaaburakatabura@gmail.com>
2022-05-27 22:47:47 +0900

    DM メッセージID指定

--- twitter_dm_get_id.php
+++ twitter_dm_get_id.php
@@ -1,8 +1,10 @@
 <?php
 $data = json_decode(file_get_contents("../local_data/twitter_data.json"),true); // jsonファイルの読み込み（非ハードコード化）
-//$base_url='https://api.twitter.com/1.1/direct_messages.json';
 
 $signature_key = rawurlencode( $data["api_secret"] )."&".rawurlencode( $data["access_token_secret"] );
+
+$base_url='https://api.twitter.com/1.1/direct_messages/events/show.json';
+$queryData = ["id"=>rawurlencode($data['message_id'])];
 
 $paramData = [
     "oauth_token" => rawurlencode( $data["access_token"] ),
@@ -11,19 +13,21 @@ $paramData = [
     "oauth_timestamp" => time(),
     "oauth_nonce" => rawurlencode(microtime()), // rawurlencode()でエンコードする必要あり
     //"oauth_nonce" => microtime(),
-    "oauth_version" => rawurlencode( "1.0" )
+    "oauth_version" => rawurlencode( "1.0" ),
 ];
+$paramData=array_merge($paramData,$queryData);
 ksort( $paramData );
 
-$url = "https://api.twitter.com/1.1/direct_messages/events/list.json";  
+$url = $base_url.'?'.http_build_query( $queryData , "", "&" );
+echo '<br>url: ';
+var_dump($url);
 
 $sig_param = rawurlencode('GET')."&".
-  //rawurlencode('https://api.twitter.com/1.1/direct_messages/events/list.json')."&".
-  rawurlencode($url)."&".
+  rawurlencode($base_url)."&".
   rawurlencode( http_build_query( $paramData , "", "&" ) );
 
-//echo '<br>';
-//var_dump($sig_param);
+echo '<br><br>';
+var_dump($sig_param);
 
 $signature = hash_hmac( "sha1", $sig_param, $signature_key, TRUE );
 $signature = base64_encode( $signature );
@@ -34,27 +38,23 @@ $httpHeader = [
     'content-type: application/json'
 ];
 
-//var_dump($httpHeader);
-//echo '<br>';
+echo '<br><br>';
+var_dump($httpHeader);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeader);
-//curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/1.1/direct_messages/events/list.json');
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, rawurlencode('GET'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-//echo '<br>httpHeader: ';
-//var_dump($httpHeader);
 
 $json = curl_exec($ch);
 $response = json_decode($json, true);
 curl_close($ch);
 
-//echo '<br>';
+echo '<br><br><br>';
 var_dump( $response );
 echo '<br><br>';
-
+/*
 $events=$response['events'];
 foreach($events as $event){
     //$name = $result['includes']['users'][$i]['name'];
@@ -65,3 +65,4 @@ foreach($events as $event){
     echo $text;
     echo '</p>';
   }
+*/
